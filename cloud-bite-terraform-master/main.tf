a5a6212a94fd3c67525eddde8b26905e29612d11
LaugeSolvang <lasol21@student.sdu.dk>
2023-10-10 08:39:50 +0200

    added backend and bucket.

--- cloud-bite-terraform-master/main.tf
+++ cloud-bite-terraform-master/main.tf
@@ -5,85 +5,32 @@ terraform {
             source = "hashicorp/google"
             version = "4.51.0"
         }
-    } 
+    }
+
     backend "gcs" {
-        bucket = "tfstate-cloud-buite-sdu"
+        bucket = "tfstate-cloud-bite-sdu-13"
         prefix = "terraform/state"
     }
 }
 
-provider "google" {
-    project = "ccc-gr13-f23"
-    region = "europe-west1"
-    zone = "europe-west1-a"
+resource "google_compute_network" "vpc_network" {
+  name = "terraform2-network"
 }
 
-
 resource "google_storage_bucket" "default" {
-    name = "tfstate-cloud.bite-sdu"
-    force_destroy ="false"
+    name = "tfstate-cloud-bite-sdu-13"
     location = "EU"
+    force_destroy = false
     storage_class = "STANDARD"
     versioning {
         enabled = true
     }
+    public_access_prevention = "enforced"
 }
 
-# Create a GCP project
-resource "google_project" "my_project" {
-  name       = "my-project-name"
-  project_id = "my-project-id"
-  org_id     = "your-organization-id" # Optional: If applicable
+
+provider "google" {
+    project = "ccc-gr13-f23"
+    region = "europe-west1"
+    zone = "europe-west1-b"
 }
-
-# Enable necessary APIs
-resource "google_project_service" "source_repo" {
-  project = google_project.my_project.project_id
-  service = "sourcerepo.googleapis.com"
-}
-
-resource "google_project_service" "secret_manager" {
-  project = google_project.my_project.project_id
-  service = "secretmanager.googleapis.com"
-}
-
-resource "google_project_service" "sqladmin" {
-  project = google_project.my_project.project_id
-  service = "sqladmin.googleapis.com"
-}
-
-# Create a Google Cloud Source Repository git repo
-resource "google_sourcerepo_repository" "my_repo" {
-  name     = "my-repo-name"
-  project  = google_project.my_project.project_id
-}
-
-# Set up IAM roles and invite team members
-# Define your IAM roles and bindings here
-
-# Create a Terraform landing zone (if needed)
-# Initialize Terraform backend (e.g., Google Cloud Storage)
-
-# Create build pipeline for frontend and backend applications
-# Implement your build pipeline configurations here
-
-# Deploy frontend application to Storage Bucket with CDN
-# Implement your deployment configurations here
-
-# Deploy backend application to Cloud Run
-# Implement your deployment configurations here
-
-# Use Secret Manager to store database sensitive information
-# Implement Secret Manager configurations here
-
-# Create Cloud SQL MySQL instance
-# Implement Cloud SQL configurations here
-
-# Create secure connection between Cloud Run backend and MySQL instance
-# Implement secure connection configurations here
-
-# Implement generative AI and associated resources
-# Implement generative AI configurations here
-
-# Implement centralised logging and alerting
-# Implement centralized logging and alerting configurations here
