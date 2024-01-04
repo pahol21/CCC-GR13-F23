9723fed7a761ef35fd45ebd4c23b26a6958df0ac
LaugeSolvang <lasol21@student.sdu.dk>
2024-01-04 19:58:00 +0100

    Passing them as arguments

--- cloud-bite-backend-master/src/app.module.ts
+++ cloud-bite-backend-master/src/app.module.ts
@@ -1,22 +1,27 @@
-import { Module } from "@nestjs/common";
+import { Module, DynamicModule } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { OrderModule } from "./order/order.module";
 import { Order } from "./order/order.entity";
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: process.env.DB_HOST, 
-      port: 3306,
-      username: 'admin_user', 
-      password: 'admin_password', 
-      database: 'my-database', 
-      entities: [Order],
-      synchronize: true,
-      retryAttempts: 5,
-    }),
-    OrderModule,
-  ],
-})
-export class AppModule {}
+@Module({})
+export class AppModule {
+  static forRoot(dbConfig: any): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [
+        TypeOrmModule.forRoot({
+          type: "mysql",
+          host: dbConfig.host, 
+          port: 3306,
+          username: dbConfig.username, 
+          password: dbConfig.password, 
+          database: dbConfig.database, 
+          entities: [Order],
+          synchronize: true,
+          retryAttempts: 5,
+        }),
+        OrderModule,
+      ],
+    };
+  }
+}
--- cloud-bite-backend-master/src/main.ts
+++ cloud-bite-backend-master/src/main.ts
@@ -10,24 +10,19 @@ async function getSecret(project, key) {
 }
 
 async function bootstrap() {
-  const dbHost = await getSecret('ccc-gr13-f23', "db-host");
-  const dbUser = await getSecret('ccc-gr13-f23', "db-username");
-  const dbPassword = await getSecret('ccc-gr13-f23', "db-password");
-  const dbName = await getSecret('ccc-gr13-f23', "db-database");
+  const dbConfig = {
+    host: await getSecret('ccc-gr13-f23', "db-host"),
+    username: await getSecret('ccc-gr13-f23', "db-username"),
+    password: await getSecret('ccc-gr13-f23', "db-password"),
+    database: await getSecret('ccc-gr13-f23', "db-database"),
+  };
 
-  process.env.DB_HOST = dbHost;
-  process.env.DB_USER = dbUser;
-  process.env.DB_PASSWORD = dbPassword;
-  process.env.DB_NAME = dbName;
+  console.log('Database configuration set:');
+  console.log(dbConfig);
 
-  console.log('Environment variables set:');
-  console.log(`DB_HOST: ${process.env.DB_HOST}`);
-  console.log(`DB_USER: ${process.env.DB_USER}`);
-  console.log(`DB_PASSWORD: ${process.env.DB_PASSWORD}`);
-  console.log(`DB_NAME: ${process.env.DB_NAME}`);
-
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule.forRoot(dbConfig));
   app.enableCors({ origin: "*" });
   await app.listen(3000);
 }
+
 bootstrap();
