ee3ef4b08be2ee75911ae7fa7f541e950c56960c
GustavBW <gustavbw@gmail.com>
2024-01-09 16:25:46 +0100

    Centralized logging... maybe?

--- cloud-bite-terraform-master/0_gcs-config.tf
+++ cloud-bite-terraform-master/0_gcs-config.tf
@@ -18,3 +18,29 @@ provider "google" {
     region = "europe-west1"
     zone = "europe-west1-b"
 }
+
+resource "google_bigquery_dataset" "combined_logs" {
+  dataset_id                  = "combined_logs"
+  friendly_name               = "Combined Logs For All Services"
+  description                 = "A dataset for storing logs"
+  location                    = "EU"  // Specify the region
+  project                     = "ccc-gr13-f23"
+  // Additional options can be set according to your requirements.
+}
+
+resource "google_logging_project_sink" "top_level_sink" {
+  name           = "project-sink"
+  destination    = "bigquery.googleapis.com/projects/${google_bigquery_dataset.combined_logs.project}/datasets/${google_bigquery_dataset.combined_logs.dataset_id}"
+  filter         = "resource.type=\"cloud_run_revision\" OR resource.type=\"storage.googleapis.com/Bucket\" OR resource.type=\"your_database_resource_type\""
+
+  unique_writer_identity = true
+}
+
+resource "google_project_iam_binding" "bigquery_data_editor_binding" {
+  project                     = "ccc-gr13-f23"
+  role                        = "roles/bigquery.dataEditor"
+
+  members = [
+    "serviceAccount:${google_logging_project_sink.my_sink.writer_identity}"
+  ]
+}
