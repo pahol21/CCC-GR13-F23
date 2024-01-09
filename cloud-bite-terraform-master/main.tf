51628f4ee46549529e44f7a2f0dc657c1df9523b
GustavBW <gustavbw@gmail.com>
2024-01-09 13:22:10 +0100

    Update main.tf

--- cloud-bite-terraform-master/main.tf
+++ cloud-bite-terraform-master/main.tf
@@ -75,6 +75,32 @@ resource "google_storage_bucket_iam_binding" "public_read" {
   ]
 }
 
+
+#Images
+resource "google_storage_bucket" "frontend_images_bucket" {
+  name          = "cloud-frontend-images-bucket"
+  location      = "EU"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+resource "google_storage_bucket_iam_binding" "public_read" {
+  bucket = google_storage_bucket.frontend_images_bucket.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "allUsers",
+  ]
+}
+
+output "frontend_images_bucket_url" {
+  value = "http://${google_storage_bucket.frontend_images_bucket.name}.storage.googleapis.com"
+}
+
 # Output the website URL
 output "website_url" {
   value = "http://${google_storage_bucket.frontend_bucket.name}.storage.googleapis.com"
