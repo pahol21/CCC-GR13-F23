68f1ebf9698208b1fe1ab30139a81865f49ba5ab
LaugeSolvang <lasol21@student.sdu.dk>
2024-01-10 20:48:32 +0100

    Deleted prior database

--- cloud-bite-terraform-master/7_db.tf
+++ cloud-bite-terraform-master/7_db.tf
@@ -1,37 +1,3 @@
-# Cloud SQL Database Instance
-resource "google_sql_database_instance" "default" {
-  name             = "my-database-instance"
-  region           = "europe-west1"
-  database_version = "MYSQL_5_7"
-
-  settings {
-    tier = "db-f1-micro"    
-    ip_configuration {
-      ipv4_enabled = true
-      authorized_networks {
-        name  = "Allow all inbound"
-        value = "0.0.0.0/0"
-      }
-    }
-  }
-  lifecycle {
-        ignore_changes = [settings[0].insights_config]
-    }
-}
-
-# Cloud SQL Database
-resource "google_sql_database" "default" {
-  name     = "order-database"
-  instance = google_sql_database_instance.default.name
-}
-
-# Cloud SQL User
-resource "google_sql_user" "users" {
-  name     = "admin_user"
-  instance = google_sql_database_instance.default.name
-  password = "admin_password"
-}
-
 # Cloud SQL Database Instance (Second Instance)
 resource "google_sql_database_instance" "second_instance" {
   depends_on = [google_service_networking_connection.private_vpc_connection]
